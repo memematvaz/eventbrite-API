dbd0503d2ef2eeac40feefea5506db8a37e942ca
memematvaz <memematvaz@gmail.com>
2020-06-01 15:49:02 +0200

    textSearch

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 const eventbrite = new EventBrite;
 
 const ui = new Interface;
+
+const searchBtn = document.getElementById('searchBtn')
+
+
+
+search = (e) => {
+    e.preventDefault();
+
+    const textSearch = document.getElementById('event').value;
+
+    const categoriesSelect = document.getElementById('category-list');
+    const categorySelected = categoriesSelect.options[categoriesSelect.selectedIndex].value;
+
+    if(textSearch) {
+        console.log('searching')
+    } else { console.log('no hay nada')}
+}
+
+searchBtn.addEventListener('click', search)
