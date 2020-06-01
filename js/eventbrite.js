a611dbb6ab0b3ca08423f1970ff079523fdff85b
memematvaz <memematvaz@gmail.com>
2020-06-01 14:12:28 +0200

    select options from api

--- js/eventbrite.js
+++ js/eventbrite.js
@@ -5,7 +5,11 @@ class EventBrite {
     }
 
     async getCategories() {
-        
+        const categoriesApi = await fetch(`https://www.eventbriteapi.com/v3/categories/?token=${this.token_auth}`);
+
+        const categoriesJSON = await categoriesApi.json();
+
+        return categoriesJSON;
     }
 
 }
--- js/interface.js
+++ js/interface.js
@@ -6,10 +6,23 @@ class Interface {
     }
 
     init(){
-
+        this.printCategories();
     }
 
     printCategories(){
-        const categoriesList = eventbrite.getCategories();
+        const categoriesList = eventbrite.getCategories()
+            .then(categoriesJson => {
+                console.log(categoriesJson.categories)
+                const categories = categoriesJson.categories;
+                
+                const selectCategories = document.getElementById('category-list')
+
+                categories.forEach(category => {
+                    const option = document.createElement('option');
+                    option.value = category.id;
+                    option.appendChild(document.createTextNode(category.name_localized))
+                    selectCategories.appendChild(option);
+                })
+            })
     }
 }
